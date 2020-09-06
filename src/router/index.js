8518708b5092ad1d77efdfce517e892b37099791
ChingFeng <hcforsch@gmail.com>
2020-09-06 17:57:47 +0800

    新增管理UI,Menu選單

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import dataStore from "../store";
+import demo from "../view/demo";
 import login from "../view/login";
 
 Vue.use(VueRouter);
@@ -9,12 +11,31 @@ const router = new VueRouter({
     mode: "hash",
     routes: [
         { path: "/login", name: "logIn", component: login },
+        { path: "/", component: demo, meta: { requireAuth: true},
+            children:[
+
+            ]
+        }
+
     ]
 });
 
 //授權判斷
 router.beforeEach(async (to, from, next) => {
-    next();
+    if (to.matched.some(record => record.meta.requireAuth)){
+        if (dataStore.state.auth.authenticated){
+            next();
+        }else{
+            next({
+                path: "/login",
+                query: {
+                    redirect: to.fullPath
+                }
+            });
+        }
+    }else{
+        next();
+    }
 });
 
 export default router;
